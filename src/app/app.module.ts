3db54f4a9d61dae5045a3109b7ddc6e63280d1df
Ramon <239752020@eniac.edu.br>
2021-09-28 00:12:10 -0300

    Adicionado Bootstrap ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaginaComponent } from './componentes/pagina/pagina.component';
+import { HomepageComponent } from './componentes/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PaginaComponent
+    PaginaComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
