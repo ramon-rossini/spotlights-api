d7dbb6a08bcf14ee044984f96b09b31159a70f20
Ramon <239752020@eniac.edu.br>
2021-10-30 17:49:19 -0300

    nmp atualizado e confirmacao de cadastro v1 completado

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn } from '@angular/forms';
+
 import { UsuariosService } from 'src/app/usuarios.service';
 import { UsuarioModel } from './usuario.model';
 
@@ -9,15 +11,27 @@ import { UsuarioModel } from './usuario.model';
 })
 export class HeaderComponent implements OnInit {
 
+  //API
   usuario: UsuarioModel = new UsuarioModel();
   usuarios: Array<any> = new Array();
 
-  constructor( private usuariosService: UsuariosService ) { }
+  myForm!: FormGroup //Formulário de cadastro
+
+  constructor(private usuariosService: UsuariosService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.listarUsuarios();
+
+    //Formulário de Cadastro
+    this.myForm = this.fb.group({
+      password: this.fb.control(null),
+      confirmPassword: this.fb.control(null),
+    }, {
+      validators: this.controlValuesAreEqual('password', 'confirmPassword')
+    })
   }
 
+  //API
   atualizar(id: number){
     this.usuariosService.atualizarUsuario(id, this.usuario).subscribe(usuario => {
       this.usuario = new UsuarioModel();
@@ -27,6 +41,7 @@ export class HeaderComponent implements OnInit {
     })
   }
 
+  //API
   remover(id: number){
     this.usuariosService.removerUsuario(id).subscribe(usuario => {
       this.usuario = new UsuarioModel();
@@ -36,6 +51,7 @@ export class HeaderComponent implements OnInit {
     })
   }
 
+  //API
   cadastrar(){
     console.log(this.usuario);
     this.usuariosService.cadastrarUsuario(this.usuario).subscribe(usuario => {
@@ -46,12 +62,28 @@ export class HeaderComponent implements OnInit {
     })
   }
 
+  //API
   listarUsuarios(){
     this.usuariosService.listarUsuarios().subscribe(usuarios => {
       this.usuarios = usuarios;
     }, err => {
       console.log("Erro ao listar os usuarios", err);
     })
+  }
+
+  //Formulário de Cadastro
+  private controlValuesAreEqual(controlNameA: string, controlNameB: string): ValidatorFn{
+    return (control: AbstractControl): ValidationErrors | null => {
+      const formGroup = control as FormGroup
+      const valueOfControlA = formGroup.get(controlNameA)?.value
+      const valueOfControlB = formGroup.get(controlNameB)?.value
+      
+      if (valueOfControlA === valueOfControlB) {
+        return null
+      } else {
+        return { valuesDoNotMatch: true }
+      }
+    }
   }
 
 }
